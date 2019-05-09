6afcd0853d416b00421d020e88b51e941a084387
Tommy Ettinger <tommy.ettinger@gmail.com>
2019-05-08 18:53:08 -0700

    Going to try Artemis-ODB here...
    Not totally clear on how to start, though.

---
+++ core/src/main/java/com/github/tommyettinger/dl/component/Position.java
@@ -0,0 +1,11 @@
+package com.github.tommyettinger.dl.component;
+
+import com.artemis.Component;
+
+/**
+ * Created by Tommy Ettinger on 5/8/2019.
+ */
+public class Position extends Component {
+    public int x;
+    public int y;
+}
